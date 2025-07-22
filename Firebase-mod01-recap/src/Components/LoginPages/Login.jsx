e85aafd945a2a4c4095a3e45d6e6b582964bf371
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-23 02:18:05 +0600

    updated file

--- Firebase-mod01-recap/src/Components/LoginPages/Login.jsx
+++ Firebase-mod01-recap/src/Components/LoginPages/Login.jsx
@@ -1,10 +1,17 @@
-import { GoogleAuthProvider } from "firebase/auth";
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../firebase.config";
 const Login = () => {
     // declared google provider
     const provider = new GoogleAuthProvider();
     const handleSigninWithGoogle = () => {
-        console.log('wait');
+        // integrate sign in method
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     return (
         <div className="hero bg-base-200 min-h-screen">
