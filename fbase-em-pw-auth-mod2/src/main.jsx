f7b2662c26795f33d0d4f04d6d81f244f00e8f47
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-24 21:01:43 +0600

    added new file

---
+++ fbase-em-pw-auth-mod2/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './Layouts/MainPage/Root'
+import Home from './Components/HomePage/Home'
+import Login from './Components/Pages/Login'
+import SignUp from './Components/Pages/SignUp'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/signup',
+        element: <SignUp></SignUp>
+      }
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
