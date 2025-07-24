de7d3e629e7b254f44edd10c9d62c1b8e2f13bb9
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-24 22:47:30 +0600

    added signup field data

--- fbase-em-pw-auth-mod2/src/Components/Pages/SignUp.jsx
+++ fbase-em-pw-auth-mod2/src/Components/Pages/SignUp.jsx
@@ -1,5 +1,13 @@
 
 const SignUp = () => {
+    const handleSubmit2 = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const userName = e.target.userName.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, userName, email, password);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col">
@@ -10,20 +18,22 @@ const SignUp = () => {
                     </p>
                 </div>
                 <div className="card bg-cyan-600 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label font-semibold">Full Name</label>
-                            <input type="text" className="input" placeholder="Name" />
-                            <label className="label font-semibold">User Name</label>
-                            <input type="text" className="input" placeholder="User Name" />
-                            <label className="label font-semibold">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label font-semibold">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
-                    </div>
+                    <form onSubmit={handleSubmit2}>
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="label font-semibold">Full Name</label>
+                                <input name="name" type="text" className="input" placeholder="Name" />
+                                <label className="label font-semibold">User Name</label>
+                                <input name="userName" type="text" className="input" placeholder="User Name" />
+                                <label className="label font-semibold">Email</label>
+                                <input name="email" type="email" className="input" placeholder="Email" />
+                                <label className="label font-semibold">Password</label>
+                                <input name="password" type="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Login</button>
+                            </fieldset>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
