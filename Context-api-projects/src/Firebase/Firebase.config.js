73a0cc6e17a29c9413839a5a486b1100a1cf8df7
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-26 00:47:59 +0600

    added new projects file

---
+++ Context-api-projects/src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAMVKKfn9rFc5IAFbk2JTcTVyNYIKWeXds",
+    authDomain: "private-route-mod3.firebaseapp.com",
+    projectId: "private-route-mod3",
+    storageBucket: "private-route-mod3.firebasestorage.app",
+    messagingSenderId: "236260502636",
+    appId: "1:236260502636:web:873beb7ebc1e8357757c49"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
