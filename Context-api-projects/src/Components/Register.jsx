e9ca79be26e495a5a39dfa54860519e3fb334fdb
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-26 01:55:52 +0600

    added new file

--- Context-api-projects/src/Components/Register.jsx
+++ Context-api-projects/src/Components/Register.jsx
@@ -1,26 +1,38 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const handleSubmitRegister = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, email, password);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+            <div className="hero-content flex-col">
+                <div className="text-center">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi.
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
-                    </div>
+                    <form onSubmit={handleSubmitRegister}>
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="label">Name</label>
+                                <input name="name" type="text" className="input" placeholder="Full Name" />
+                                <label className="label">Email</label>
+                                <input name="email" type="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input name="password" type="password" className="input" placeholder="Password" />
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Register</button>
+                                <p className="text-center mt-2 font-semibold">All Ready have an accounts ! Please <Link to='/login'><span className="text-cyan-600 font-bold">Login</span></Link> </p>
+                            </fieldset>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
